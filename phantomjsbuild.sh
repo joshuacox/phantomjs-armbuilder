93187eec6ef318a446e041e1b6a891e9fa1e48da
Josh Cox <josh@webhosting.coop>
2015-12-13 19:13:48 -0600

    noRM

--- phantomjsbuild.sh
+++ phantomjsbuild.sh
@@ -5,11 +5,11 @@ sudo apt-get -y install build-essential g++ flex bison gperf ruby perl \
   libpng-dev libjpeg-dev
 TMP=$(mktemp -d -t tmp.XXXXXXXXXX)
 cd $TMP
-echo "localphantom version $LOCAL_PHANTOM_VERSION" 
+echo "localphantom version $LOCAL_PHANTOM_VERSION"
 git clone git://github.com/ariya/phantomjs.git
 cd phantomjs
 git checkout 2.0
 yes|./build.sh --jobs 6
 bin/phantomjs --version
 cp bin/phantomjs /usr/local/bin/
-rm -Rf $TMP
+#rm -Rf $TMP
